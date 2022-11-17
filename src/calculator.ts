987197cb4cfbda4fb00e030f663163903fa0e79b
soymusta <mustaelhachmimahti@gmail.com>
2022-11-17 10:05:55 +0100

    roman convert

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,5 +1,20 @@
 export class Calculator {
     value: number
+    lookup = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+      };
 
     constructor() {
         this.value = 0
@@ -9,4 +24,15 @@ export class Calculator {
         this.value += n
         return this.value
     }
+    convertToRoman(num: number) {
+        let roman = '';
+        let i;
+        for (i in this.lookup) {
+          while (num >= this.lookup[i]) {
+            roman += i;
+            num -= this.lookup[i];
+          }
+        }
+        return roman;
+      } 
 }
--- test/calculator.ts
+++ test/calculator.ts
@@ -3,33 +3,43 @@ import {expect} from 'chai'
 
 describe('calculator', () => {
     it('should initialise with a calculated value of 0', () => {
-        let calculator = new Calculator()
+        const calculator = new Calculator()
         expect(calculator.value).to.equal(0)
     })
 
     it('should return a result of 5 when performing an addition of 5 immediately after creation', () => {
-        let calculator = new Calculator()
+        const calculator = new Calculator()
         expect(calculator.add(5)).to.equal(5)
     })
 
     it('should store the result of an addition performed immediately after creation as the new calculated value', () => {
-        let calculator = new Calculator()
+        const calculator = new Calculator()
         calculator.add(9)
         expect(calculator.value).to.equal(9)
     })
 
     it('should allow multiple additions to be performed, returning the result of the final additon', () => {
-        let calculator = new Calculator()
+        const calculator = new Calculator()
         calculator.add(9)
         calculator.add(2)
         expect(calculator.add(1)).to.equal(12)
     })
 
     it('should store the result of multiple additions as the new calculated value', () => {
-        let calculator = new Calculator()
+        const calculator = new Calculator()
         calculator.add(5)
         calculator.add(7)
         calculator.add(3)
         expect(calculator.value).to.equal(15)
+    })
+
+    it('give number and should return correct one, 9 should return IX', () => {
+        const calculator = new Calculator()
+        expect(calculator.convertToRoman(9)).to.equal('IX')
+    })
+
+    it('give number and should return correct one, 139 should return CXXXIX', () => {
+        const calculator = new Calculator()
+        expect(calculator.convertToRoman(139)).to.equal('CXXXIX')
     })
 })
